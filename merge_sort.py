e06189506f2bb2cef56562169195bca735c19a33
Jeremy <jjuriz@gmail.com>
2021-01-12 09:08:53 -0500

    removed test file

--- merge_sort.py
+++ merge_sort.py
@@ -1,48 +1,36 @@
-# Typically split into two functions. 
-    # first function is to split the array.
-    # second function merges the split array.
+# First function - Finds the middle of the array.
+# Using the midpoint recursively calls the function to continue splitting the array. 
+# When the array length is 1 the recusrive loop breaks and returns the array.
 
 def merge_sort(array):
-
     if len(array) <= 1:
         return array
 
-    midpoint = int(len(array) / 2)
+    middle = int(len(array) // 2)
 
-    left, right = merge_sort(array[:midpoint]), merge_sort(array[midpoint:])
-
+    left, right = merge_sort(array[:middle]), merge_sort(array[middle:])
+    print(f"printing left = {left}, printing right = {right}, printing middle = {middle}", )
     return merge(left, right)
-
+    
 def merge(left, right):
+    result = [] # Stores the final sorted result
+    left_idx = right_idx = 0 # Counters
 
-    result = []
-    left_pointer = right_pointer = 0
+    while left_idx < len(left) and right_idx < len(right):
+        # print(f"this is left index = {left_idx} and this is right index = {right_idx}")
 
-    while left_pointer < len(left) and right_pointer < len(right):
-
-        if left[left_pointer] < right[right_pointer]:
-
-            result.append(left[left_pointer])
-            left_pointer += 1
-
+        if left[left_idx] < right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+            # print(f"This is printing the result inside left_idx: {result}")
         else:
-
-            result.append(right[right_pointer])
-            right_pointer += 1
-
-    result.extend(left[left_pointer:])
-    result.extend(right[right_pointer:])
+            result.append(right[right_idx])
+            right_idx += 1
+            # print(f"This is printing the result right_idx: {result}")
+            
+    result.extend(left[left_idx:])
+    result.extend(right[right_idx:])
 
     return result
 
-def main():
-    array = [5,4,3,2,1]
-    print(array)
-
-    result = merge_sort(array)
-    print(result)
-
-if __name__ == "__main__":
-    main()
-
-
+print(merge_sort([5,2,1,89,4,76,3,22,43]))
